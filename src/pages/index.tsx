5652b7639df2f17b68309a2a36d4d2fc2ba77d18
Felipe Duarte <felipesilvapac@gmail.com>
2021-11-10 17:49:40 -0300

    App next react init

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Início | </title>
+    </Head>
+    <h1>
+      Helloo
+    </h1>
+    </>
+  )
+}
