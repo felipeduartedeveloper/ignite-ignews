5af6099dbe95a2379dc71070fe816795cbf4d08e
felipeduartedelevoper <felipesilvapac@gmail.com>
2021-11-17 11:00:45 -0300

    Sign In Button style ok

--- src/Components/SignInButton/index.tsx
+++ src/Components/SignInButton/index.tsx
@@ -1,8 +1,13 @@
 import { FaGithub } from 'react-icons/fa';
 
+import styles from './styles.module.scss';
+
 export function SignInButton() {
   return (
-    <button type="button">
+    <button 
+    type="button"
+    className={styles.signInButton}
+    >
       <FaGithub />
       Sign In with Github
     </button>
