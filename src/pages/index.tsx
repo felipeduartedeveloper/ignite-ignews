1259c19748bdd8fac2e24774de6ff03894f00daf
felipeduartedelevoper <felipesilvapac@gmail.com>
2021-11-18 16:48:52 -0300

    home component style page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,26 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
+
 export default function Home() {
   return (
     <>
     <Head>
-      <title>Início | </title>
+      <title>Início | Home</title>
     </Head>
     <h1>
-      Helloo
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>🤞 Hey, Welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br/>
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/mulher.svg" alt="Girl coding" />
+      </main>
     </h1>
     </>
   )
